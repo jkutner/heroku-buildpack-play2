5ee36730e3b61ee8b5e67b0f4caa672a3c3b5f9e
Joe Kutner <jpkutner@gmail.com>
2014-06-11 13:00:18 -0500

    trying to get the compile command working with the test-app

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -6,9 +6,9 @@ DEFAULT_SBT_VERSION="0.13.5"
 DEFAULT_SBT_JAR="sbt-launch-0.11.3-2.jar"
 SBT_TEST_CACHE="/tmp/sbt-test-cache"
 
-beforeSetUp() {
-  cp -r ${BUILDPACK_HOME}/test-app/* ${BUILD_DIR}
-}
+# beforeSetUp() {
+#   cp -r ${BUILDPACK_HOME}/test-app/* ${BUILD_DIR}
+# }
 
 afterSetUp() {
   # Remove play-specific build dir in case it's already there
@@ -18,12 +18,13 @@ afterSetUp() {
 }
 
 testCompile() {
+  cp -r ${BUILDPACK_HOME}/test-app/* ${BUILD_DIR}
 
   # create `testfile`s in CACHE_DIR and later assert `compile` copied them to BUILD_DIR
   # mkdir -p ${BUILD_DIR}/.sbt_home
   mkdir -p ${CACHE_DIR}/.sbt_home
 
-  compile ${BUILD_DIR}
+  compile
 
   assertCapturedSuccess
 
@@ -37,7 +38,7 @@ testCompile() {
   # assertEquals "SBT cache should have been repacked" "" "$(diff -r ${BUILD_DIR}/.sbt_home ${CACHE_DIR}/.sbt_home)"
 
   # re-deploy
-  compile ${BUILD_DIR}
+  compile
 
   assertCapturedSuccess
   assertNotCaptured "Activator should not re-download Scala" "Getting Scala"
